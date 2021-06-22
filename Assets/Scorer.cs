1117d58a5b68170aafa9a3e03cf433e4cc056c16
skywalkerluc <lucas.182.ferreira@hotmail.com>
2021-06-21 21:52:04 -0300

    Faker colision logic

---
+++ Assets/Scorer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    [SerializeField] int hits = 0;
+    private void OnCollisionEnter(Collision other)
+    {
+        hits++;
+        Debug.Log("You've bumped into a thing this many times: " + hits);
+    }
+}
